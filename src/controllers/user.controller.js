431917ca241266ed0c2a500eb4f05abd6cf93d47
K <khayusaki@gmail.com>
2022-08-10 10:30:47 +0800

    Initial setup for USER APIs

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,23 @@
+import { loginService, registerService } from "../services/user.service";
+
+// Get all movie list with pagination
+async function login(req, res, next) {
+  try {
+    let user = await loginService(req.query);
+    return res.status(200).send(user);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// Get a movile detail of the provided id
+async function register(req, res, next) {
+  try {
+    let user = await registerService(req.body);
+    return res.status(200).send(user);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export default { login, register };
---
+++ src/services/user.service.js
@@ -0,0 +1,8 @@
+"use strict";
+import USER from "../models/user.model";
+
+async function loginService(reqInfo) {}
+
+async function registerService(reqInfo) {}
+
+export { loginService, registerService };
