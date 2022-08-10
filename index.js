ea784e54c5374f966be98845ac2a84e00ee2d307
K <khayusaki@gmail.com>
2022-08-10 15:22:54 +0800

    Install packages for auth

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv'
 import express from "express";
 import mongoose from "mongoose";
 import config from "./src/config/index";
 import bodyParser from "body-parser";
-import router from "./src/routes/movie.route";
+import path from "path";
+import glob from "glob";
 import { errorHandler } from "./src/middlewares/handlers.middleware";
 
 const app = express();
+dotenv.config();
 
 // Mongoose connection
 main().catch((err) => console.log(err));
@@ -17,9 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Routes
-app.use("/api/v1/movies", router);
+const dir = path.join(__dirname, "./src/routes/*.js");
+const routes = glob.sync(dir);
+routes.forEach((route) => {
+  require(route).default(app);
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log("Server started on port 3000");
